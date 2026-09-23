29a217803b13e2059b7de174f2a8138116956613
Shanwaz Khan <shanwazkittur@gmail.com>
2026-09-23 19:49:35 +0530

    Prepare CyberSentinel for cloud deployment

--- app/app.py
+++ app/app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from flask import (
     Flask,
     render_template,
@@ -228,9 +231,9 @@ def generate_security_report():
         download_name="CyberSentinel_Security_Report.pdf"
     )
 
-
 if __name__ == "__main__":
-
     app.run(
+        host="0.0.0.0",
+        port=5000,
         debug=True
     )
